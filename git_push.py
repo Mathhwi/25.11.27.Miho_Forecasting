f9c247a780f074b7333eee1bb850301b7d8deddb
unknown <rlarjsgnl@sogang.ac.kr>
2025-11-27 13:39:33 +0900

    Use 25_11_27.html as index

--- git_push.py
+++ git_push.py
@@ -1,13 +1,23 @@
 # -*- coding: utf-8 -*-
 import os
-import subprocess
+import shutil
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 print(f"Working directory: {os.getcwd()}")
 
+# Copy 25_11_27.html to index.html
+if os.path.exists("25_11_27.html"):
+    shutil.copy("25_11_27.html", "index.html")
+    print("[OK] Copied 25_11_27.html -> index.html")
+
+# Remove figure_gallery.html
+if os.path.exists("figure_gallery.html"):
+    os.remove("figure_gallery.html")
+    print("[OK] Removed figure_gallery.html")
+
 commands = [
     'git add .',
-    'git commit -m "Update HTML file"',
+    'git commit -m "Use 25_11_27.html as index"',
     'git push'
 ]
 
